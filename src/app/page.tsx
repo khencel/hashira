2b6d5fd97e6c72e6093d4c8360fdaf55f607d727
khencel <khenneth.alaiza@gmail.com>
2026-06-22 09:30:38 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
         spaceBetween={20}
         slidesPerView={1}
         loop={true}
-        // modules={[Autoplay]}
+        modules={[Autoplay]}
         autoplay={{ delay: 2000 }}
         speed={800}
       >
