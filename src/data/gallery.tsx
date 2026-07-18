cb12240ebb5e4454cf151c76141ef3d3ce838664
khencel <khenneth.alaiza@gmail.com>
2026-07-18 21:10:17 +0800

    update

--- src/data/gallery.tsx
+++ src/data/gallery.tsx
@@ -1,15 +1,68 @@
 export function getGallery(){
     return [
         //main office
-        { id: 1, category: "mainOffice", title: "Conference Interview Room", year: 2025, imageUrl:"/img/gallery/main/Conference_Interview-Room.jpg" },
-        { id: 2, category: "mainOffice", title: "Language Training Room", year: 2024, imageUrl:"/img/gallery/main/Language-Training-Room-2.jpg" },
-        { id: 3, category: "mainOffice", title: "Orientation Room", year: 2025, imageUrl:"/img/gallery/main/Orientation-Room.jpg" },
-        { id: 4, category: "mainOffice", title: "Processing Division Area", year: 2023, imageUrl:"/img/gallery/main/Processing-Division-Area.jpg" },
-        { id: 5, category: "mainOffice", title: "Reception Area", year: 2024, imageUrl:"/img/gallery/main/Reception-Area-1-1.jpg" },
+        { id: 1, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker1.jpeg" },
+        { id: 2, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker2.jpeg" },
+        { id: 3, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker3.jpeg" },
+        { id: 4, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker4.jpeg" },
+        { id: 5, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker5.jpeg" },
+
+        { id: 6, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker6.jpg" },
+        { id: 7, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker7.jpeg" },
+        { id: 8, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker8.jpeg" },
+        { id: 9, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker9.jpeg" },
+        { id: 10, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker10.jpeg" },
+
+        { id: 11, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker11.jpeg" },
+        { id: 12, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker12.png" },
+        { id: 13, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker13.jpeg" },
+        { id: 14, category: "ourWorkers", imageUrl:"/img/gallery/workers/worker14.jpeg" },
 
 
-        //dormitory
-        { id: 6, category: "dormitory", title: "Bedroom A", year: 2024, imageUrl:"/img/gallery/dormitory/Bedroom-A.jpg" },
-        { id: 7, category: "dormitory", title: "Bedroom B", year: 2024, imageUrl:"/img/gallery/dormitory/Bedroom-B.jpg" },
+
+        { id: 15, category: "interview", imageUrl:"/img/gallery/interview/1.jpg" },
+        { id: 16, category: "interview", imageUrl:"/img/gallery/interview/2.jpg" },
+        { id: 17, category: "interview", imageUrl:"/img/gallery/interview/3.jpg" },
+        { id: 18, category: "interview", imageUrl:"/img/gallery/interview/4.jpg" },
+        { id: 19, category: "interview", imageUrl:"/img/gallery/interview/5.jpg" },
+        { id: 20, category: "interview", imageUrl:"/img/gallery/interview/6.jpg" },
+        { id: 21, category: "interview", imageUrl:"/img/gallery/interview/7.jpg" },
+        { id: 22, category: "interview", imageUrl:"/img/gallery/interview/8.jpg" },
+        { id: 23, category: "interview", imageUrl:"/img/gallery/interview/9.jpeg" },
+        { id: 24, category: "interview", imageUrl:"/img/gallery/interview/10.jpg" },
+
+        { id: 25, category: "tradetest", imageUrl:"/img/gallery/tradeTests/1.jpg" },
+        { id: 26, category: "tradetest", imageUrl:"/img/gallery/tradeTests/2.jpg" },
+        { id: 27, category: "tradetest", imageUrl:"/img/gallery/tradeTests/3.jpg" },
+        { id: 28, category: "tradetest", imageUrl:"/img/gallery/tradeTests/4.jpg" },
+        { id: 29, category: "tradetest", imageUrl:"/img/gallery/tradeTests/5.jpg" },
+        { id: 30, category: "tradetest", imageUrl:"/img/gallery/tradeTests/6.jpg" },
+        { id: 31, category: "tradetest", imageUrl:"/img/gallery/tradeTests/7.jpg" },
+        { id: 32, category: "tradetest", imageUrl:"/img/gallery/tradeTests/8.jpg" },
+        { id: 33, category: "tradetest", imageUrl:"/img/gallery/tradeTests/9.jpg" },
+        { id: 34, category: "tradetest", imageUrl:"/img/gallery/tradeTests/10.jpg" },
+
+
+        { id: 35, category: "facilities", imageUrl:"/img/gallery/facilities/1.jpg" },
+        { id: 36, category: "facilities", imageUrl:"/img/gallery/facilities/2.jpg" },
+        { id: 37, category: "facilities", imageUrl:"/img/gallery/facilities/3.jpg" },
+        { id: 38, category: "facilities", imageUrl:"/img/gallery/facilities/4.jpg" },
+        { id: 39, category: "facilities", imageUrl:"/img/gallery/facilities/5.jpg" },
+        { id: 40, category: "facilities",  imageUrl:"/img/gallery/facilities/6.jpg" },
+        { id: 41, category: "facilities",  imageUrl:"/img/gallery/facilities/7.jpeg" },
+        { id: 42, category: "facilities",  imageUrl:"/img/gallery/facilities/8.jpeg" },
+        { id: 43, category: "facilities",  imageUrl:"/img/gallery/facilities/9.jpeg" },
+        { id: 44, category: "facilities",  imageUrl:"/img/gallery/facilities/10.jpeg" },
+
+        { id: 45, category: "others", imageUrl:"/img/gallery/others/1.jpg" },
+        { id: 46, category: "others", imageUrl:"/img/gallery/others/2.jpg" },
+        { id: 47, category: "others", imageUrl:"/img/gallery/others/3.jpeg" },
+        { id: 48, category: "others", imageUrl:"/img/gallery/others/4.jpg" },
+        { id: 49, category: "others", imageUrl:"/img/gallery/others/5.jpg" },
+        { id: 50, category: "others",  imageUrl:"/img/gallery/others/6.jpg" },
+        { id: 51, category: "others",  imageUrl:"/img/gallery/others/7.jpeg" },
+        { id: 52, category: "others",  imageUrl:"/img/gallery/others/8.jpeg" },
+        { id: 53, category: "others",  imageUrl:"/img/gallery/others/9.jpeg" },
+        { id: 54, category: "others",  imageUrl:"/img/gallery/others/10.jpeg" },
     ]
 }
