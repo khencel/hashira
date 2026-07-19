3ac8d8ca049a70c19a2378956b068d86ca2a080a
khencel <khenneth.alaiza@gmail.com>
2026-07-19 12:22:44 +0800

    update

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -33,7 +33,7 @@ export default function ContactUs(){
                             <div className="row">
                                 <div className="col-md-6">
                                     <div >
-                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30892.52672393006!2d121.15724164692706!3d14.566802681197766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c1e9d1ddc9b5%3A0xfc095f21b3b019!2sHashira%20Skills%20%26%20Language%20Academy!5e0!3m2!1sen!2sph!4v1784434116956!5m2!1sen!2sph" width="600" height="450" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="strict-origin-when-cross-origin"></iframe>
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30892.52672393006!2d121.15724164692706!3d14.566802681197766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397c1e9d1ddc9b5%3A0xfc095f21b3b019!2sHashira%20Skills%20%26%20Language%20Academy!5e0!3m2!1sen!2sph!4v1784434116956!5m2!1sen!2sph" width="600" height="450" style={{border:"0"}} allowFullScreen={true} loading="lazy" referrerPolicy="strict-origin-when-cross-origin"></iframe>
                                     </div>
                                     
                                 </div>
